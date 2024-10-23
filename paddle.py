2ee8418dc68ac04897dff108fbbaad54d1d0d4a1
DannyCurnock <162930699+DannyCurnock@users.noreply.github.com>
2024-10-23 11:38:36 +0100

    Add files via upload

---
+++ paddle.py
@@ -0,0 +1,34 @@
+# Create and move paddle 
+# width = 20
+# height = 100
+# x_pos = 350
+# y-pos = 0
+# should move up and down by 20 pixles 
+from turtle import Turtle 
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(position)
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)    
+
+
+
+   
+
+
+    
+
